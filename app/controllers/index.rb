c4297363f3fad3e6df8e1450962a47f8cbe1f8a8
Apprentice <apprentice@devbootcamp.com>
2013-09-04 17:17:06 -0700

    add route post comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,3 +7,9 @@ get '/posts' do
   @post = Post.order("created_at DESC")
   erb :posts
 end
+
+get '/posts/:post_id/comments' do
+  @post = Post.find(params[:post_id])
+  @comments = @post.comments
+  erb :post_comments
+end
