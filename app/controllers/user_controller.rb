74f3f23d2778a7b4ad9dcc5f045ba8668be67c51
Apprentice <apprentice@devbootcamp.com>
2013-09-04 19:37:15 -0700

    Added user comment history view page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,11 @@
 get '/user/:user_id' do
   @user = User.find(params[:user_id])
-  
+
   erb :user
+end
+
+get '/user/:user_id/comments' do
+  @comments = User.find(params[:user_id]).comments.order("created_at DESC")
+
+  erb :user_comments
 end
